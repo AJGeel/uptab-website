6d9ad342be590b828ed61b70977d5f4031986896
Arthur Geel <ageel@enrise.com>
2024-02-24 14:19:40 +0100

    Linter setup

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,12 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        sky: {
+          400: "#25B1FF",
+          800: "#085884",
+        },
+        black: "#020E2C",
       },
     },
   },
