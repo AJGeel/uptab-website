a1417df3eac05ef69005a9e482e5e8b6574a999e
Arthur Geel <ageel@enrise.com>
2024-03-03 10:17:57 +0100

    Add middleware to api routes to allow CORS access from select origins

--- src/app/api/background/route.ts
+++ src/app/api/background/route.ts
@@ -11,15 +11,15 @@ export const GET = async (request: NextRequest) => {
   }
 
   try {
-    const url = new URL(request.url);
-    const urlParams = url.searchParams.get("theme") ?? undefined;
-
+    const urlParams = request.nextUrl.searchParams.get("theme") ?? undefined;
     const theme = findTheme(urlParams);
     const backgroundImage = await getBackgroundImage(theme);
 
-    return new NextResponse(backgroundImage, { status: 200, statusText: "OK" });
+    return new NextResponse(backgroundImage, {
+      status: 200,
+      statusText: "OK",
+    });
   } catch (error) {
-    console.error(error);
     return new NextResponse("Internal server error", {
       status: 500,
     });
---
+++ src/middleware.ts
@@ -0,0 +1,47 @@
+import { NextRequest, NextResponse } from "next/server";
+
+const allowedOrigins = [
+  "chrome-extension://djfbncgipfgbmidgjacfcignnobfjdec",
+  "moz-extension://4879cca2-726a-4087-ad6b-41dc66f2afd1",
+];
+
+const corsOptions = {
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization, Uptab-Api-Key",
+};
+
+export const middleware = (request: NextRequest) => {
+  // Check the origin from the request
+  const origin = request.headers.get("origin") ?? "";
+  const isAllowedOrigin = allowedOrigins.includes(origin);
+
+  // Handle preflighted requests
+  const isPreflight = request.method === "OPTIONS";
+
+  if (isPreflight) {
+    const preflightHeaders = {
+      ...(isAllowedOrigin && {
+        "Access-Control-Allow-Origin": origin,
+      }),
+      ...corsOptions,
+    };
+    return NextResponse.json({}, { headers: preflightHeaders });
+  }
+
+  // Handle simple requests
+  const response = NextResponse.next();
+
+  if (isAllowedOrigin) {
+    response.headers.set("Access-Control-Allow-Origin", origin);
+  }
+
+  Object.entries(corsOptions).forEach(([key, value]) => {
+    response.headers.set(key, value);
+  });
+
+  return response;
+};
+
+export const config = {
+  matcher: "/api/:path*",
+};
