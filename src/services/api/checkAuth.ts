74ee58af61aacbe6d3b7bff42198f1dce6c275c2
Arthur Geel <ageel@enrise.com>
2024-03-03 09:52:38 +0100

    Use NextUrl Searchparams

--- src/services/api/checkAuth.ts
+++ src/services/api/checkAuth.ts
@@ -2,4 +2,5 @@ import { NextRequest } from "next/server";
 
 export const checkAuth = (request: NextRequest) =>
   process.env.NODE_ENV === "development" ||
-  request.headers.get("UPTAB_API_KEY") === process.env.UPTAB_API_KEY;
+  request.nextUrl.searchParams.get("UPTAB_API_KEY") ===
+    process.env.UPTAB_API_KEY;
